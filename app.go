8f451da2efbd6f59679bd390e91f2c8ee1dfc39b
Waldemar Quevedo <wally@synadia.com>
2021-04-02 16:27:32 -0700

    App without server dependency
    Signed-off-by: Waldemar Quevedo <wally@synadia.com>

--- app.go
+++ app.go
@@ -1,11 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/nats-io/nats.go"
+	"log"
 )
 
 func main() {
-	fmt.Println("foo")
-	nats.Connect("demo.nats.io")
+	nc, err := nats.Connect("demo.nats.io")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doneCh := make(chan struct{})
+	nc.Subscribe("foo", func(msg *nats.Msg) {
+		log.Println("[Received]", string(msg.Data))
+		doneCh <- struct{}{}
+	})
+	nc.Publish("foo", []byte("Hello Go modules!"))
+
+	<-doneCh
 }
